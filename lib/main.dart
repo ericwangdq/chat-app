6b1456fa5aaee31daf50f4df6265df3ffd07cc51
Eric Wang <eric.wang04@sap.com>
2025-11-21 22:32:33 +0800

    add ios

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chat App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      // theme: ThemeData(primarySwatch: Colors.blue),
+      theme:
+          ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.white)),
       home: const LoginScreen(),
     );
   }
